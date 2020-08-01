8eaf005c28bd12aa4a4cb614af6ab26eb465ff43
Karim <karimelzanaty98@gmail.com>
2020-08-01 16:01:33 -0400

    Golem complete

---
+++ IslandWish/IslandWishGame/Assets/Code/Enemy/MeleeAttackCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttackCollision : MonoBehaviour
+{
+	[SerializeField] int damage = 0;
+
+	public void InitDamage(int newDamage)
+	{
+		damage = newDamage;
+	}
+
+	private void FixedUpdate()
+	{
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			print("HIT");
+
+			//DamageEvent damageEvent = new DamageEvent(damage);
+			//
+			//EventManager.instance.FireEvent(damageEvent);
+		}
+	}
+}
