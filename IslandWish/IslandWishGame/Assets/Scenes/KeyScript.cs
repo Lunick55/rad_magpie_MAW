7e1e96f1aae8a89552ff5d54670adf474d2dfc2c
Petra Jarrett <petra.jarrett@mymail.champlain.edu>
2020-08-04 02:08:47 -0400

    Final greybox version of level 1 for alpha

---
+++ IslandWish/IslandWishGame/Assets/Scenes/KeyScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour
+{
+
+    public GameObject door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(door);
+            Destroy(gameObject);
+        }
+    }
+}
